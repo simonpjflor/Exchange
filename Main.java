220c47b8ef2ad99f882d6f4e352d469221a70a69
J_Atuesta <spatuesta@gmail.com>
2024-10-22 17:02:38 -0500

    migrando

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("este es el repo en github para el challenge, trasladando el trabajo del repo en gitlab");
     }
 }
