51d1e0df2adfee7c601517ddcd97642eab780821
J_Atuesta <spatuesta@gmail.com>
2024-11-10 00:00:57 -0500

    setters & getters

--- Main.java
+++ Main.java
@@ -1,10 +1,29 @@
 public class Main {
+    private float inputValue = 0.0f;
+    private float outputValue = 0.0f;
+
     public static void main(String[] args) {
-<<<<<<< HEAD
-        System.out.println("Git init");
-=======
-        System.out.println("Hello world!");
-        System.out.println("este es el repo en github para el challenge, trasladando el trabajo del repo en gitlab");
->>>>>>> origin/main
+    //entrada
+        System.out.println("conversor COP a USD");
+        System.out.println("Ingrese la cantidad a convertir");
+
     }
+
+
+    public float getInputValue(){
+        return inputValue;
+    }
+
+    public float getOutputValue() {
+        return outputValue;
+    }
+
+    public void setInputValue(float inputValue) {
+        this.inputValue = inputValue;
+    }
+
+    public void setOutputValue(float outputValue) {
+        this.outputValue = outputValue;
+    }
+
 }
