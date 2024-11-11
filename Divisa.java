af62c1cd767a5398dbaddae3a6f2fa6836d72384
J_Atuesta <spatuesta@gmail.com>
2024-11-10 21:49:34 -0500

    classes created

---
+++ Divisa.java
@@ -0,0 +1,23 @@
+public class Divisa {
+
+    private float inputValue = 0.0f;
+    private float outputValue = 0.0f;
+
+    public float getInputValue(){
+        return inputValue;
+    }
+
+    public float getOutputValue() {
+        return outputValue;
+    }
+
+
+    public void setInputValue(float inputValue) {
+        this.inputValue = inputValue;
+    }
+
+    public void setOutputValue(float outputValue) {
+        this.outputValue = outputValue;
+    }
+
+}
--- Main.java
+++ Main.java
@@ -1,31 +1,21 @@
+/*
+Main, instancia la clase divisas y llama a la Api
+*/
 import java.util.Scanner;
 public class Main {
-    private float inputValue = 0.0f;
-    private float outputValue = 0.0f;
+
+
     public static void main(String[] args) {
-    //Entrada
+        //Entrada
+        Divisa cantidadConvertir = new Divisa();
+
         System.out.println("conversor COP a USD");
-        System.out.println("Ingrese la cantidad a convertir");
+        System.out.println("Ingrese la cantidad  a convertir");
         Scanner consoleInput = new Scanner(System.in);
-        thi.setInputValue(consoleInput.nextFloat());
+        cantidadConvertir.setInputValue(consoleInput.nextFloat());// hace falta una excepcion?
+        //llamar API
+
     }
 
-
-    public float getInputValue(){
-        return inputValue;
-    }
-
-    public float getOutputValue() {
-        return outputValue;
-    }
-
-
-    public void setInputValue(float inputValue) {
-        this.inputValue = inputValue;
-    }
-
-    public void setOutputValue(float outputValue) {
-        this.outputValue = outputValue;
-    }
 
 }
