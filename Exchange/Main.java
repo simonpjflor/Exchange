76e564a9a6e959d5d366aee4aef75acc1181a11f
J_Atuesta <spatuesta@gmail.com>
2024-12-28 21:29:35 -0500

    Branch created to store previouse version

--- Exchange/Main.java
+++ Exchange/Main.java
@@ -3,7 +3,6 @@ Main//Gets inputs//calls methods
 */
 package Exchange;
 import com.google.gson.Gson;
-
 import java.io.IOException;
 import java.util.Scanner;
 public class Main {
@@ -15,7 +14,6 @@ public class Main {
         float amount = consoleInput.nextFloat();
         System.out.println("Ingrese la divisa actual");
         String initialCurrency = consoleInput.next();
-//
         Gson gson = new Gson();
         Jason JsonObject = gson.fromJson(API.apiConsume(initialCurrency),Jason.class);
         Printing.Validation(JsonObject,amount, initialCurrency);
