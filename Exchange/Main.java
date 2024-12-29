8d571e642315b62429a5ea01b8313f4cc3e6faf2
J_Atuesta <spatuesta@gmail.com>
2024-12-28 19:57:53 -0500

    cleaning main//rewriting Printing method to add amount and use the JsonObjects from gson

--- Exchange/Main.java
+++ Exchange/Main.java
@@ -18,27 +18,9 @@ public class Main {
         String initialCurrency = consoleInput.next();
         System.out.println("Ingrese la divisa deseada");
         String desiredCurrency = consoleInput.next();
-
 // Using JsonObjects
         JsonObject jsonObject = JsonParser.parseString(API.apiConsume(initialCurrency)).getAsJsonObject();
         JsonObject conversionRates = jsonObject.getAsJsonObject("conversion_rates");
         Printing.Validation(conversionRates,amount, initialCurrency, desiredCurrency);
-
-
-        String[] currencies = {"USD","EUR","JPY","GBP","AUD"};
-        String[] topCurrencies = new String[7];
-        int i =2;
-        if (conversionRates.has(desiredCurrency)){
-            topCurrencies[0]="1 "+initialCurrency+" = 1 "+desiredCurrency+" : "+conversionRates.get(desiredCurrency).getAsFloat()+"\n";
-            topCurrencies[1]="\nMost Traded Currencies\n";
-            }
-        for (String currency : currencies) {//looks each top currency component in the Json
-            if (conversionRates.has(currency)) {
-                float rate = conversionRates.get(currency).getAsFloat();
-                topCurrencies[i]=currency+" : "+rate+"\n";
-                i++;
-            }
-        }
-        System.out.println(Arrays.toString(topCurrencies));
     }
 }
--- Exchange/Printing.java
+++ Exchange/Printing.java
@@ -3,24 +3,41 @@ package Exchange;
 
 import com.google.gson.JsonObject;
 
+import java.util.Arrays;
+
 public class Printing {
+
     public static void Validation(JsonObject conversionRates, float amount, String initialCurrency, String desiredCurrency){
 
         if (amount <0){
             amount =-amount;
         }if (amount ==0 || amount ==1) {
-            Printing.ToString(conversionRates,initialCurrency);
+            Printing.unitPrinting(conversionRates,initialCurrency,desiredCurrency);
         }else{
-            Printing.ToString(conversionRates,initialCurrency,amount);
-            Printing.ToString(conversionRates,initialCurrency);
+            Printing.amountPrinting(conversionRates,initialCurrency,desiredCurrency,amount);
+            Printing.unitPrinting(conversionRates,initialCurrency,desiredCurrency);
         }
     }
-    public static void ToString(JsonObject conversionRates, String initialCurrency ){
-        System.out.printf("\nresult: "+conversionRates+"\n");
+    public static void unitPrinting(JsonObject conversionRates, String initialCurrency, String desiredCurrency ){
+        String[] currencies = {"USD","EUR","JPY","GBP","AUD"};
+        String[] topCurrencies = new String[7];
+        int i =2;
+        if (conversionRates.has(desiredCurrency)){
+            topCurrencies[0]="1 "+initialCurrency+" = 1 "+desiredCurrency+" : "+conversionRates.get(desiredCurrency).getAsFloat()+"\n";
+            topCurrencies[1]="\nMost Traded Currencies\n";
+        }
+        for (String currency : currencies) {//looks each top currency component in the Json
+            if (conversionRates.has(currency)) {
+                float rate = conversionRates.get(currency).getAsFloat();
+                topCurrencies[i]=currency+" : "+rate+"\n";
+                i++;
+            }
+        }
+        System.out.println(Arrays.toString(topCurrencies));
 
     }
 
-    public static void ToString(JsonObject conversionRates, String initialCurrency, float amount){
+    public static void amountPrinting(JsonObject conversionRates, String initialCurrency,String desiredCurrency, float amount){
         System.out.printf("\nresult: "+conversionRates+"\n");
 
     }
